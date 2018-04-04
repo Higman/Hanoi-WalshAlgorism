d64d95e23f2367fc7aeaa28154117be79df0fb82
Higman <s15t201@stu.kagawa-u.ac.jp>
2018-04-04 23:49:50 +0900

    実行するアルゴリズムの選択を可能にした

--- src/main/kotlin/com/github/Higman/BaseAnchorPane.kt
+++ src/main/kotlin/com/github/Higman/BaseAnchorPane.kt
@@ -1,30 +1,30 @@
 package com.github.Higman
 
-import com.github.Higman.hanoi.Hanoi
-import com.github.Higman.hanoi.HanoiTowerAlgorithm
+import com.github.Higman.hanoi.*
 import javafx.event.ActionEvent
 import javafx.fxml.FXML
 import javafx.fxml.FXMLLoader
 import javafx.fxml.Initializable
-import javafx.scene.control.Button
-import javafx.scene.control.Label
-import javafx.scene.control.Spinner
-import javafx.scene.control.SpinnerValueFactory
+import javafx.scene.control.*
 import javafx.scene.layout.BorderPane
 import javafx.scene.layout.FlowPane
 import javafx.scene.layout.Pane
 import java.net.URL
 import java.util.*
+import kotlin.reflect.KFunction
 
-class BaseAnchorPane : BorderPane(), Initializable {
+class BaseAnchorPane : BorderPane() {
     @FXML lateinit var label: Label
     @FXML lateinit var diskSpinner: Spinner<Int>
     @FXML lateinit var controllerFlowPane: FlowPane
     @FXML lateinit var drawPane: Pane
     @FXML lateinit var controllButton: Button
+    @FXML lateinit var hanoiWalshRadioButton: RadioButton
+    @FXML lateinit var towerRadioButton: RadioButton
 
     private var isStartedHanoi = false
     private lateinit var hanoiDrawer: HanoiDrawer
+    private lateinit var toggleGroup: ToggleGroup
 
     init {
         val fxmlLoader: FXMLLoader = FXMLLoader(
@@ -32,10 +32,15 @@ class BaseAnchorPane : BorderPane(), Initializable {
         fxmlLoader.setRoot(this)
         fxmlLoader.setController(this)
         fxmlLoader.load<BorderPane>()
+
+        initializeComponent()
     }
 
-    override fun initialize(location: URL?, resources: ResourceBundle?) {
+    private fun initializeComponent() {
         diskSpinner.valueFactory = SpinnerValueFactory.IntegerSpinnerValueFactory(3, 10, 5)
+        toggleGroup = hanoiWalshRadioButton.toggleGroup
+        hanoiWalshRadioButton.userData = ::HanoiWalshAlgorithm
+        towerRadioButton.userData = ::HanoiTowerAlgorithm
     }
 
     @FXML
@@ -44,7 +49,10 @@ class BaseAnchorPane : BorderPane(), Initializable {
             isStartedHanoi = !isStartedHanoi
             controllButton.text = "次へ"
             drawPane.children.clear()
-            hanoiDrawer = HanoiDrawer(HanoiTowerAlgorithm(Hanoi(diskSpinner.value)),  drawPane)
+
+            val execAlgorithm = toggleGroup.selectedToggle.userData as Function1<Hanoi, HanoiTowerAlgorithm>
+            val hanoi = Hanoi(diskSpinner.value)
+            hanoiDrawer = HanoiDrawer(execAlgorithm(hanoi),  drawPane)
         } else {
             if (hanoiDrawer.next()) {
                 isStartedHanoi = !isStartedHanoi
